122de4b8a6efa6fc54094fea3bb7d6ce371b8f75
Unknown <Stormstrike87@gmail.com>
2018-05-02 15:28:06 +0200

    Merge

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -7,7 +7,6 @@ public class Main {
 	public static void main(String[] args) {
 		new InitializeChess();
 		
-		//new ChessBoard(false, false, "localhost", 21337, "Sjakk"); //Singleplayer versjon for debugging
 	}
 
 }
